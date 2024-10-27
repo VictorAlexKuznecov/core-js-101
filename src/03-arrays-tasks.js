6d3cfdade6224f786f8b7a1425257fb6d801e63e
Victor <victoralexkuznecov@gmail.com>
2024-10-27 22:31:37 +0300

    feat:add solutions 2

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -422,10 +422,14 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr.toSorted((a, b) => {
+    if (a.country === b.country) {
+      return a.city.localeCompare(b.city);
+    }
+    return a.country.localeCompare(b.country);
+  });
 }
-
 /**
  * Creates an identity matrix of the specified size
  *
@@ -444,8 +448,12 @@ function sortCitiesArray(/* arr */) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  const arr = new Array(n).fill(0).map(() => new Array(n).fill(0));
+  return arr.map((item, index) => {
+    item.splice(index, 1, 1);
+    return item;
+  });
 }
 
 /**
@@ -461,8 +469,13 @@ function getIdentityMatrix(/* n */) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return new Array(end - start + 1).fill(start).reduce((acc, item, index) => {
+    if (index === 0) {
+      return [item];
+    }
+    return [...acc, item + index];
+  }, []);
 }
 
 /**
@@ -476,8 +489,8 @@ function getIntervalArray(/* start, end */) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return arr.filter((item, index, array) => index === array.indexOf(item));
 }
 
 /**
